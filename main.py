b5763eaf18a6d570a67b9ccb3ff5a898554a8fe9
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-14 20:13:32 -0700

    main combines both scripts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from picamzero import Camera
+from time import sleep
 from pi5neo import Pi5Neo
-import time
 import sys
-
 
 SPI_DEVICE = '/dev/spidev0.0' # Rpi protocol to get the timing right for the GPIOs
 SPI_SPEED_KHZ = 800 #speed of SPI protocol
@@ -9,8 +9,10 @@ SPI_SPEED_KHZ = 800 #speed of SPI protocol
 neo = Pi5Neo(SPI_DEVICE, 30, SPI_SPEED_KHZ)
 
 # Fill the strip with white (R,G,B = 255,255,255)
+
 neo.fill_strip(255, 255, 255)
 neo.update_strip()  # commit/send to LEDs
-
-
-
+cam = Camera()
+cam.start_preview()
+# Keep the preview window open for 5 seconds
+sleep(5)

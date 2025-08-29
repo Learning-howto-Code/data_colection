0e77a7f643459152260862b5d76c15c514d4119a
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-28 19:44:07 -0700

    added timestamps

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from picamzero import Camera
 from time import sleep
 from pi5neo import Pi5Neo
-import sys
+from time import strftime
+
+img_folder= "/home/jake/Downloads/data_collection/images"
+filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S')}.jpg"
 
 SPI_DEVICE = '/dev/spidev0.0' # Rpi protocol to get the timing right for the GPIOs
 SPI_SPEED_KHZ = 800 #speed of SPI protocol
@@ -15,7 +18,7 @@ def take_pic():
     neo.update_strip()  #sends command
     cam = Camera()
     cam.start_preview()
-    cam.take_photo("img.jpg")
+    cam.take_photo(filename)
     cam.stop_preview()
     neo.fill_strip(0, 0, 0) #sets LED's to black
 

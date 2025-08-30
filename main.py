48d28acd48429f60ceefef10ed783baa79f45853
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-29 20:09:54 -0700

    closed camera process after taking pic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from picamzero import Camera
-from time import sleep
+from time import sleep, strftime
 from pi5neo import Pi5Neo
-from time import strftime
 
 img_folder= "/home/jake/Downloads/data_collection/images"
 filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S')}.jpg"
@@ -14,10 +13,11 @@ neo = Pi5Neo(SPI_DEVICE, pin, SPI_SPEED_KHZ)
 
 def take_pic():
 
-    neo.fill_strip(255, 255, 255) #sets color to pure white
+    neo.fill_strip(200, 200, 200) #sets color to pure white
     neo.update_strip()  #sends command
     cam = Camera()
     cam.take_photo(filename)
+    cam.close()
     neo.fill_strip(0, 0, 0) #sets LED's to black
 
 take_pic()

93d831613d1ac4f0c72d1c8c00e5e3fe1583b6fc
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-30 14:04:14 -0700

    trying new way

--- test.py
+++ test.py
@@ -1,26 +1,9 @@
-from picamera2 import Picamera2
-from time import sleep, strftime
-from pi5neo import Pi5Neo
-import os
-
-# Image folder
-img_folder = "/home/jake/Downloads/data_collection/images"
-
-# SPI setup for NeoPixel
-SPI_DEVICE = '/dev/spidev0.0'
-SPI_SPEED_KHZ = 800
-pin = 30
-neo = Pi5Neo(SPI_DEVICE, pin, SPI_SPEED_KHZ)
-
-def take_pic():
-    filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S')}.jpg" #names pic as timestamp
-    neo.fill_strip(200, 200, 200) #sets LED's to white and a little dimmer
-    neo.update_strip()
-    picam2 = Picamera2()
-    sleep(1)  #waits for cam to calibrate
-    picam2.capture_file(filename) 
-    del picam2 #stops worker funtion
-    neo.fill_strip(0, 0, 0)
-    neo.update_strip()
-
-take_pic()
+from picamera2 import Picamera2, Preview
+import time
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")

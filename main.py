b12d4871b646623b62631ec07d46853576314e31
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-29 20:17:47 -0700

    moved a var

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep, strftime
 from pi5neo import Pi5Neo
 
 img_folder= "/home/jake/Downloads/data_collection/images"
-filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S')}.jpg"
+
 
 SPI_DEVICE = '/dev/spidev0.0' # Rpi protocol to get the timing right for the GPIOs
 SPI_SPEED_KHZ = 800 #speed of SPI protocol
@@ -12,7 +12,7 @@ pin = 30
 neo = Pi5Neo(SPI_DEVICE, pin, SPI_SPEED_KHZ)
 
 def take_pic():
-
+    filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S')}.jpg"
     neo.fill_strip(200, 200, 200) #sets color to pure white
     neo.update_strip()  #sends command
     cam = Camera()

ba5d4993cd7dc0114092b02679619e780d71ee05
Learning-howto-Code <jaketorohopkins@icloud.com>
2025-08-30 14:25:32 -0700

    added test script

--- test.py
+++ test.py
@@ -1,9 +1,34 @@
-from picamera2 import Picamera2, Preview
-import time
+from pi5neo import Pi5Neo
+from time import sleep
+import sys
+
+
+SPI_DEVICE = '/dev/spidev0.0' # Rpi protocol to get the timing right for the GPIOs
+SPI_SPEED_KHZ = 800 #speed of SPI protocol
+
+neo = Pi5Neo(SPI_DEVICE, 30, SPI_SPEED_KHZ)
+
+# Fill the strip with white (R,G,B = 255,255,255)
+neo.fill_strip(255, 255, 255)
+neo.update_strip()  # commit/send to LEDs
+sleep(60)
+neo.fill_strip(0, 0, 0)
+neo.update_strip() 
+
+from picamera2 import Picamera2
+
+# Create camera instance
 picam2 = Picamera2()
-camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-picam2.configure(camera_config)
-# picam2.start_preview(Preview.QTGL)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+
+# Configure preview
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
+
+# Start preview
+picam2.start(show_preview=True)
+
+input("Press Enter to stop preview...")
+
+picam2.stop()
+
+

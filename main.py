13f96e5b114137fee7467784096c9a588f57adf2
Jake Hopkins <151584990+Learning-howto-Code@users.noreply.github.com>
2025-09-11 16:29:16 +0000

    images only are taken for 30 seconds

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from picamera2 import Picamera2
-from time import sleep, strftime
+from time import sleep, strftime, time
 from pi5neo import Pi5Neo
 import os
 
@@ -11,33 +11,27 @@ SPI_DEVICE = '/dev/spidev0.0'
 SPI_SPEED_KHZ = 800
 pin = 30
 neo = Pi5Neo(SPI_DEVICE, pin, SPI_SPEED_KHZ)
-
+ 
 def take_pic():
     
     neo.fill_strip(10, 10, 10) #sets LED's to white and a little dimmer
-    neo.update_strip()
+    neo.update_strip() #sets color
     print("light on")
     picam2 = Picamera2()
     config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-    picam2.configure(config)
+    picam2.configure(config) #sets configuration
     picam2.start()
     print("activated cam")
-    sleep(2)
-    print("waited 2 sec")
+    sleep(2)    #watis for cam to start
+    print("waited 2 sec") #cam is now ready
 
-    try:
-        while True:
-            filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S_%f')}.jpg"   #names pic as timestamp
-            picam2.capture_file(filename)
-            print("took pic")
-            sleep(.2)
+    start_time = time() 
+    while time() - start_time < 30: # only takes iamges for 30 seconds
+        filename = f"{img_folder}/img_{strftime('%Y%m%d_%H%M%S_%f')}.jpg"   #names pic as timestamp
+        picam2.capture_file(filename)
+        print("took pic")
+        sleep(.2)
 
-    finally:
-        picam2.close()
-        del picam2 #stops worker funtion
-        print("stopped cam")
-        neo.fill_strip(0, 0, 0)
-        neo.update_strip()
-        print("turned off light")
+   
 
 take_pic()

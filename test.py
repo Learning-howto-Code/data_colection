2d7c4f4c12fcd6230137e06058ad0ca78c7270cb
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-01-23 17:03:00 -0800

    created test scripto view live feed

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from pi5neo import Pi5Neo
 from time import sleep
+from picamzero import Camera
+
+cam = Camera()
+cam.start_preview()
 
 neo = Pi5Neo('/dev/spidev0.0', 30, 250)
 
 neo.fill_strip(255, 255, 255)
 neo.update_strip()
-sleep(5)
+sleep(100)
 
 neo.fill_strip(0, 0, 0)
 neo.update_strip()
+cam.end_preview()
